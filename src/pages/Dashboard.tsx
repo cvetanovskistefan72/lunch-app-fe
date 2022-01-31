ae2e16a87067a3846650bb63eb4de07955058dde
Stefan Cvetanovski <cvetanovskistefan72@gmail.com>
2022-01-31 21:20:57 +0100

    Create Dashboard component / Create Dashboard scss

---
+++ src/pages/Dashboard.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import styles from './Dashboard.module.scss';
+interface Props  {
+
+};
+
+const Dashboard:React.FC<Props> = () => {
+  return <div className={styles.root}></div>;
+};
+
+export default Dashboard;
